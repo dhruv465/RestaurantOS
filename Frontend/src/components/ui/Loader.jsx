0db1535a24f4ac0cd1875186f958c836c6178a31
dhruv465 <dhruv.sathe11@gmail.com>
2025-02-23 23:02:52 +0530

    Update loader styling and add theme support to loader component

--- Frontend/src/components/ui/Loader.jsx
+++ Frontend/src/components/ui/Loader.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import { useTheme } from '../../context/ThemeContext'
 
 const Loader = () => {
+  const { theme } = useTheme()
+
   return (
-    <div className='fullscreen-loader'>
-        <div className="spinner">
-            
-        </div>
-      
+    <div className={`fullscreen-loader ${theme}`}>
+      <div className="spinner"></div>
     </div>
   )
 }
