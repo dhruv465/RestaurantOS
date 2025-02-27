dd1aa1e79ce40dd4b0d0b853acf9d67acf0649b6
dhruv465 <dhruv.sathe11@gmail.com>
2025-02-27 21:30:04 +0530

    Adjust Orders page layout with dynamic height calculation

--- Frontend/src/pages/Orders.jsx
+++ Frontend/src/pages/Orders.jsx
@@ -67,7 +67,7 @@ const Orders = () => {
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 px-4 md:px-8 lg:px-16 py-4 overflow-y-auto  scrollbar-hide">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 px-4 md:px-8 lg:px-16 py-4 overflow-y-auto h-[calc(100vh-5rem-7rem)] scrollbar-hide">
         {" "}
         {/* h-[calc(100vh-5rem-7rem)] */}
         {resData?.data.data.length > 0 ? (
