9b899d8ba218c1af89d99f1c3bcc23be82818398
dhruv465 <dhruv.sathe11@gmail.com>
2025-03-01 15:01:02 +0530

    Update CORS origin to Render deployment URL

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true ,
-    origin: ['http://localhost:5173']
+    origin: ['https://restos.onrender.com']
 }))
 
 //Endpoint Root
