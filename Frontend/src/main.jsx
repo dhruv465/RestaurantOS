57ee8d7fdd683b63014472f9b8faa262a8e3bafc
dhruv465 <dhruv.sathe11@gmail.com>
2025-03-06 17:11:45 +0530

    Add React Query DevTools for improved development debugging

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,26 +3,27 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store.js";
 import  { SnackbarProvider} from "notistack"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      
       staleTime: 30000,
     },
   },
 })
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <SnackbarProvider maxSnack={3} autoHideDuration={3000} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}> {/* Wrap App with PersistGate */}
+            <App /> 
+          </PersistGate>
         </QueryClientProvider>
       </SnackbarProvider>
     </Provider>
