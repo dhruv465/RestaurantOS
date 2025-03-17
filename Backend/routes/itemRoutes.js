5492cd672a9deb273f9312e8d8882eb353c52b3d
dhruv465 <dhruv.sathe11@gmail.com>
2025-03-17 23:42:46 +0530

    Add new route for fetching popular dishes in itemRoutes, integrating getPopularDishes function from itemController to enhance API capabilities.

--- Backend/routes/itemRoutes.js
+++ Backend/routes/itemRoutes.js
@@ -5,7 +5,8 @@ const {
     getItems,
     getItemById,
     updateItem,
-    deleteItem
+    deleteItem,
+    getPopularDishes // Importing the new function
 } = require('../controllers/itemController');
 const { isVerifiedUser } = require('../middlewares/tokenVerification');
 
@@ -13,6 +14,8 @@ router.route('/').post(isVerifiedUser, createItem);
 router.route('/').get(getItems);
 router.route('/:id').get(getItemById);
 router.route('/:id').put(isVerifiedUser, updateItem);
+router.route('/popular-dishes').get(getPopularDishes); // New route for fetching popular dishes
+router.route('/popular-dishes').get(getPopularDishes); // New route for fetching popular dishes
 router.route('/:id').delete(isVerifiedUser, deleteItem);
 
 module.exports = router;
